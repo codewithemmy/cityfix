8138a8f4987f30bee71f32cb35ada10b7674a3ee
codewithemmy <codewithemmy@gmail.com>
2023-07-22 11:44:27 +0100

    updates

--- files/user/user.repository.js
+++ files/user/user.repository.js
@@ -3,15 +3,17 @@ const mongoose = require("mongoose")
 
 class UserRepository {
   static async create(payload) {
-    return User.create(payload)
+    return await User.create(payload)
   }
 
   static async findUserWithParams(userPayload, select) {
-    return User.find({ ...userPayload }).select(select)
+    return await User.find({ ...userPayload }).select(select)
   }
 
   static async findSingleUserWithParams(userPayload, select) {
-    return User.findOne({ ...userPayload }).select(select)
+    const user = await User.findOne({ ...userPayload }).select(select)
+
+    return user
   }
 
   static async validateUser(userPayload) {
