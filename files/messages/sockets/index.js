8d05cd9a23bff5da9e213f73d4162cdda0480ff3
codewithemmy <codewithemmy@gmail.com>
2023-08-03 21:31:14 +0100

    dkdkd

--- files/messages/sockets/index.js
+++ files/messages/sockets/index.js
@@ -4,7 +4,6 @@ const fs = require("fs")
 
 module.exports.socketConnection = async (io) => {
   io.on("connection", async (socket) => {
-    console.log(`⚡: ${socket.id} user just connected!`)
     socket.on("join", async (obj) => {
       try {
         //check to delete the socket id with the userId
