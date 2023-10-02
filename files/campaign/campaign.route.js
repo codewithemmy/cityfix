4b79f034943b0f3c2b0e0c1fc5fba33d05052bf6
codewithemmy <codewithemmy@gmail.com>
2023-10-02 16:44:10 +0100

    update: getting campaign

--- files/campaign/campaign.route.js
+++ files/campaign/campaign.route.js
@@ -2,9 +2,9 @@ const campaignRoute = require("express").Router()
 const { isAuthenticated } = require("../../utils")
 const { getCampaignController } = require("./campaign.controller")
 
+campaignRoute.route("/").get(getCampaignController)
 campaignRoute.use(isAuthenticated)
 
 //routes
-campaignRoute.route("/").get(getCampaignController)
 
 module.exports = campaignRoute
